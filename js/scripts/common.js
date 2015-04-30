dcff88e9db6a7ff22151c6a78b07e26f464aec95
Artyom Kayun <kayun.artem@gmail.com>
2015-04-30 22:07:19 +0700

    oled anim

--- js/scripts/common.js
+++ js/scripts/common.js
@@ -1,4 +1,21 @@
-
+if (!device.mobile() || !device.tablet() || !isIE8() || !isIE7()) {
 	var scroll = skrollr.init();
-	console.log("ok")
+}
+
+
+function isIE8() {
+	if (document.all && document.querySelector && !document.addEventListener) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isIE7() {
+	if (document.all && !document.querySelector) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
