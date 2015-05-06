95446386e193ca371b6a0bd965cfea107cb91bfb
Artyom Kayun <kayun.artem@gmail.com>
2015-05-06 19:37:38 +0700

    fix btn

--- js/scripts/ultra.js
+++ js/scripts/ultra.js
@@ -10,14 +10,14 @@ $window.load(function () {
 		$document.bind('scroll', function () {
 
 			if ($colorPrimeSection.position().top +
-				$colorPrimeSection.innerHeight() + 200 <= $document.scrollTop() +
+				$colorPrimeSection.innerHeight() <= $document.scrollTop() +
 				$window.height()) {
 
 				$checkbox.attr('checked', 'checked')
 			}
 
 			if ($colorPrimeSection.position().top +
-				$colorPrimeSection.innerHeight() + 200 >= $document.scrollTop() +
+				$colorPrimeSection.innerHeight() >= $document.scrollTop() +
 				$window.height()) {
 
 				$checkbox.removeAttr('checked')
