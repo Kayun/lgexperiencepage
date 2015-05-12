11f2c9d8e58838c978913e5caef7f35993443328
Artyom Kayun <kayun.artem@gmail.com>
2015-05-12 15:25:39 +0700

    fix mobile

---
+++ dist/assets/scripts/common.js
@@ -0,0 +1,28 @@
+if (!device.mobile() && !device.tablet() && !isIE8() && !isIE7()) {
+	var scroll = skrollr.init();
+}
+
+if (isIE8() || isIE7()) {
+	var $imgBgCover = $('.js-bg-cover');
+
+	$imgBgCover.each(function () {
+		$(this).css('background-size', 'cover');
+	});
+}
+
+function isIE8() {
+	if (document.all && document.querySelector && !document.addEventListener) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isIE7() {
+	if (document.all && !document.querySelector) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
